03c4ffa2dfdf26437d837a47a7b2dc3195e2de22
Leopold <leopold.goudier@gmail.com>
2022-01-19 15:56:51 +0100

    test

---
+++ main.py
@@ -0,0 +1,12 @@
+import tweepy as tw
+
+auth = tw.OAuthHandler("zMnm4cb1pqpDkvHiuR23OtnIc", "dBfiSHyU3hGC4aHQSlpUCvMrCnlEBJJiV942D0YiMSJhybJWW1")
+auth.set_access_token("1265572530640879616-fdWmNdQ1FyiTlThfJTXsRXRm80QLCA", "ykRX16GXGsEvUHXSlF5HKnHSOxH3Gli7l2KTOflAwar8n")
+
+api = tw.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Connected Bot")
+except:
+    print("Launch Error")
