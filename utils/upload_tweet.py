f535d2057fdd478a8d30990094594629b74fa0e7
Leopold <leopold.goudier@gmail.com>
2022-02-23 21:14:46 +0100

    add sentences part

--- utils/upload_tweet.py
+++ utils/upload_tweet.py
@@ -1,3 +1,6 @@
+import json
+import random
+
 def upload(api, musicOfDay):
     """Start by uploading the mp4 video, then add the description containing the day, title, artist, album, link, and date. 
     Finally, post the tweet, along with the description and media.
@@ -15,5 +18,10 @@ def upload(api, musicOfDay):
     link = musicOfDay[0].link
     date = musicOfDay[0].release_date
 
-    tweet = f"Day {day}\n\nTitle : {title}\nArtist : {artist}\nAlbum : {album}\nLink : {link}\nDate : {date}\n\n#music #deezer"
+    with open('utils/data/sentences.json', "r") as file :
+        file = json.load(file)
+
+    sentence = random.choice(file)
+
+    tweet = f"Day {day}\n\n🎵 Title : {title}\n👨‍🎤 Artist : {artist}\n🎸 Album : {album}\n🔗 Link : {link}\n📆 Date : {date}\n\n🤟 {sentence}\n#music #deezer"
     api.update_status(status=tweet, media_ids=[media.media_id])
