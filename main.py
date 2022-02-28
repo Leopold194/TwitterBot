d37c1e02e27f54d99bf4f7b4355b5714bc290db9
Leopold <leopold.goudier@gmail.com>
2022-02-28 20:24:38 +0100

    Add Help for Error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tweepy as tw
 import os
 import time
+import smtplib, ssl
 
 from keep_alive import keep_alive
 from datetime import datetime
@@ -10,11 +11,12 @@ from utils.get_functional_video import create_functional_video
 from utils.track_of_day import get_trackOfDay
 from utils.get_audio_image import get_audio, get_image
 
-consumer_key = "Y0w1V5Fx08E9YVwb8yeEQZkTJ"
-consumer_secret = "tbXHOsf1DFyhxzr2YePJzIz3BXWkIJ2GHk0Zx8YsRnNkfnyhGq"
+consumer_key = "YOUR CONSUMER KEY"
+consumer_secret = "YOUR CONSUMER KEY SECRET"
 
-access_token = "1494014284761907203-fDqNtiqF7sJ8S5Xy5hdcnxSXcMVUl0"
-access_token_secret = "nhcarTONHrDHEWvgbyw1G7uhSq2feubhICba4M3eDrRBT"
+access_token = "YOUR ACCESS TOKEN"
+access_token_secret = "YOUR ACCESS TOKEN SECRET"
+
 """Used to connect to the Twitter client
 """
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
@@ -31,45 +33,54 @@ except:
 
 keep_alive()
 while connect:
-		"""Check if it's time to post the tweet, especially here 6am
-		"""
-		actually_time = datetime.now()
-		if actually_time.hour + 1 == 6 and actually_time.minute == 0:
+	"""Check if it's time to post the tweet, especially here 6am"""
+	
+	actually_time = datetime.now()
+	if actually_time.hour + 1 == 6 and actually_time.minute == 0:
+		"""Start by recovering the sound of the day, then isolate the link and the cover of this music. Then create mp4 video which will contain audio and cover. Finally, post the tweet, and delete the files that are no longer needed."""
+		try:
 
-				"""Start by recovering the sound of the day, then isolate the link and the cover of this music. Then create mp4 video which will contain audio and cover. Finally, post the tweet, and delete the files that are no longer needed."""
+			musicOfDay = get_trackOfDay()
+
+			link_video = musicOfDay[0].link
+			link_cover = musicOfDay[0].album["cover_xl"]
+
+			get_audio(link_video)		
+			get_image(link_cover)
+
+			files = False
+			while files == False:
 				try:
-						musicOfDay = get_trackOfDay()
-						link_video = musicOfDay[0].link
-						link_cover = musicOfDay[0].album["cover_xl"]
-		
-						get_audio(link_video)		
-						get_image(link_cover)
-		
-						files = False
-						while files == False:
-								try:
-										with open('uploads/songOfDay.mp3'): pass
-										with open('uploads/short_audio.mp3'): pass
-										with open('uploads/coverOfDay.jpg'): pass
-										files = True
-								except:
-										time.sleep(2)
-		
-						create_functional_video()
-		
-						tweet = upload(api, musicOfDay)
-				
-						if tweet == 1:
-								print("Tweet posted")
-								os.remove('uploads/video.mp4')
-								os.remove('uploads/video.avi')
-								os.remove('uploads/coverOfDay.jpg')
-								os.remove('uploads/songOfDay.mp3')
-								os.remove('uploads/short_audio.mp3')
-				
+					with open('uploads/songOfDay.mp3'): pass
+					with open('uploads/short_audio.mp3'): pass
+					with open('uploads/coverOfDay.jpg'): pass
+					files = True
 				except:
+					time.sleep(2)
 
-						print(f"Error in posting --> {actually_time}")
-		else:
-				
-				time.sleep(30)
+			time.sleep(30)
+			create_functional_video()
+
+			tweet = upload(api, musicOfDay)
+
+			if tweet == 1:
+				print("Tweet posted")
+				os.remove('uploads/video.mp4')
+				os.remove('uploads/video.avi')
+				os.remove('uploads/coverOfDay.jpg')
+				os.remove('uploads/songOfDay.mp3')
+				os.remove('uploads/short_audio.mp3')
+
+		except Exception as Error:
+			smtp_address = 'smtp.gmail.com'
+			smtp_port    = "PORT (INT)"
+			smtp_context = ssl.create_default_context()
+			email        = "EMAIL"
+			password     = "PASSWORD"
+
+		with smtplib.SMTP_SSL(smtp_address, smtp_port, context=smtp_context) as server:
+			server.login(email, password) 
+			server.sendmail(email, "EMAIL THAT RECEIVES THE ERROR MAIL", str(Error))
+
+	else:
+		time.sleep(30)
--- utils/upload_tweet.py
+++ utils/upload_tweet.py
@@ -2,7 +2,7 @@ import json
 import random
 import time
 
-def upload(api, musicOfDay):
+def upload(api, musicOfDay, exception = False):
     """Start by uploading the mp4 video, then add the description containing the day, title, artist, album, link, and date. 
     Finally, post the tweet, along with the description and media.
     
@@ -10,23 +10,23 @@ def upload(api, musicOfDay):
         api : client Twitter
         musicOfDay (dict): all informations about music of day
     """
-    time.sleep(60)
+    time.sleep(150)
 	
     media = api.media_upload(filename='uploads/video.mp4')
-        
-    title = musicOfDay[0].title_short
-    artist = musicOfDay[0].artist["name"]
-    album = musicOfDay[0].album["title"]
-    day = musicOfDay[1]
-    link = musicOfDay[0].link
-    date = musicOfDay[0].release_date
-
+    
     with open('utils/data/sentences.json', "r") as file :
         file = json.load(file)
 
     sentence = random.choice(file)
-	
-    tweet = f"Day {day}\n\n🎵 Title : {title}\n👨‍🎤 Artist : {artist}\n🎸 Album : {album}\n🔗 Link : {link}\n📆 Date : {date}\n\n🤟 {sentence}\n#music #deezer"
 
+    title = musicOfDay[0].title_short
+    artist = musicOfDay[0].artist["name"]
+    album = musicOfDay[0].album["title"]
+    link = musicOfDay[0].link
+    date = musicOfDay[0].release_date
+    day = musicOfDay[1]
+
+    tweet = f"Day {day}\n\n🎵 Title : {title}\n👨‍🎤 Artist : {artist}\n🎸 Album : {album}\n🔗 Link : {link}\n📆 Date : {date}\n\n🤟 {sentence}\n#music #deezer"
+   
     post = api.update_status(status=tweet, media_ids=[media.media_id])
     return 1
