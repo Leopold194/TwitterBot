55f29738f7b8111088beb8dbd82f5b7e6a11a9df
Leopold <leopold.goudier@gmail.com>
2022-02-28 20:24:49 +0100

    Fix Bug

--- utils/get_functional_video.py
+++ utils/get_functional_video.py
@@ -9,6 +9,6 @@ def create_functional_video():
     """
     vg.make_video()
 
-    time.sleep(60)
+    time.sleep(150)
 			
     os.popen("ffmpeg -i uploads/video.avi -vf scale=-2:480,format=yuv420p -c:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -threads 0 -c:a aac -c:v libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 uploads/video.mp4")
