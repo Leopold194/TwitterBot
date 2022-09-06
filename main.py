4dffc9ad73eeea88b4f25cc265abb5db4055c312
Leopold194 <46968525+Leopold194@users.noreply.github.com>
2022-09-06 15:48:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,12 @@ from utils.get_functional_video import create_functional_video
 from utils.track_of_day import get_trackOfDay
 from utils.get_audio_image import get_audio, get_image
 
-<<<<<<< HEAD
-consumer_key = "Y0w1V5Fx08E9YVwb8yeEQZkTJ"
-consumer_secret = "tbXHOsf1DFyhxzr2YePJzIz3BXWkIJ2GHk0Zx8YsRnNkfnyhGq"
-=======
 consumer_key = "YOUR CONSUMER KEY"
 consumer_secret = "YOUR CONSUMER KEY SECRET"
 
 access_token = "YOUR ACCESS TOKEN"
 access_token_secret = "YOUR ACCESS TOKEN SECRET"
->>>>>>> origin/main
 
-access_token = "1494014284761907203-fDqNtiqF7sJ8S5Xy5hdcnxSXcMVUl0"
-access_token_secret = "nhcarTONHrDHEWvgbyw1G7uhSq2feubhICba4M3eDrRBT"
 """Used to connect to the Twitter client
 """
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
@@ -40,68 +33,8 @@ except:
 
 keep_alive()
 while connect:
-<<<<<<< HEAD
-    """Check if it's time to post the tweet, especially here 6am"""
-    actually_time = datetime.now()
-    if (actually_time.hour + 2 == 5 and actually_time.minute == 54):
-        """Start by recovering theWhat is your favorite song from the last 4? sound of the day, then isolate the link and the cover of this music. Then create mp4 video which will contain audio and cover. Finally, post the tweet, and delete the files that are no longer needed.
-        """
-        try:
-
-            while os.listdir('uploads') != []:
-    	        for filename in os.listdir('uploads'):
-                    os.remove("uploads/" + filename)
-
-            musicOfDay = get_trackOfDay()
-            
-            link_video = musicOfDay[0].link
-            link_cover = musicOfDay[0].album["cover_xl"]
-
-            get_audio(link_video)
-            get_image(link_cover)
-
-            files = False
-            while files == False:
-                try:
-                    with open('uploads/songOfDay.mp3'):
-                        pass
-                    with open('uploads/short_audio.mp3'):
-                        pass
-                    with open('uploads/coverOfDay.jpg'):
-                        pass
-                    files = True
-                except:
-                    time.sleep(2)
-
-            time.sleep(30)
-            create_functional_video()
-
-            tweet = upload(api, musicOfDay)
-
-            if tweet == 1:
-                print("Tweet posted")
-                
-            time.sleep(30)
-
-        except Exception as Error:
-            smtp_address = "smtp.gmail.com"
-            smtp_port = 465
-            smtp_context = ssl.create_default_context()
-            email = "leopold.goudier@gmail.com"
-            password = "naruto&yugi!"
-
-            with smtplib.SMTP_SSL(smtp_address,
-                                  smtp_port,
-                                  context=smtp_context) as server:
-                server.login(email, password)
-                server.sendmail(email, "botdylan.tweeter@gmail.com",
-                                str(Error))
-
-    else:
-        time.sleep(30)
-=======
 	"""Check if it's time to post the tweet, especially here 6am"""
-	
+
 	actually_time = datetime.now()
 	if actually_time.hour + 1 == 6 and actually_time.minute == 0:
 		"""Start by recovering the sound of the day, then isolate the link and the cover of this music. Then create mp4 video which will contain audio and cover. Finally, post the tweet, and delete the files that are no longer needed."""
@@ -151,4 +84,3 @@ while connect:
 
 	else:
 		time.sleep(30)
->>>>>>> origin/main
